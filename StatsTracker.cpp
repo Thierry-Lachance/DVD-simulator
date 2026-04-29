5c7016894d07364fb2d00e1de3084d3ea5adb6e1
Thierry Lachance <lachancethierry6@gmail.com>
2026-04-29 16:15:49 +0000

    stats

--- StatsTracker.cpp
+++ StatsTracker.cpp
@@ -1,5 +1,29 @@
 //
-// Created by lxs on 4/27/26.
+// Created by thierry on 4/27/26.
 //
 
 #include "StatsTracker.h"
+
+StatsTracker::StatsTracker(){
+
+    wallHit = new int[4];
+}
+
+void StatsTracker::logWallHit(int wallNumber){
+    wallHit[wallNumber]++;
+}
+
+int StatsTracker::getTotalNumberOfHit(){
+    return wallHit[0] + wallHit[1] + wallHit[2] + wallHit[3];
+}
+
+int StatsTracker::getNumberOfHitForWall(int wallNumber){
+    return wallHit[wallNumber];
+}
+
+void StatsTracker::logCornerHit(int cornerNumber){
+    cornerHit[cornerNumber]++;
+}
+int StatsTracker::getNumberOfHitForCorner(int cornerNumber){
+    return cornerHit[cornerNumber];
+}
--- StatsTracker.h
+++ StatsTracker.h
@@ -8,6 +8,18 @@
 
 
 class StatsTracker {
+    private:
+        int[] wallHit;
+        int[] cornerHit;
+    public:
+        StatsTracker();
+        void logWallHit(int wallNumber);
+        int getTotalNumberOfHit();
+        int getNumberOfHitForWall(int wallNumber);
+
+        void logCornerHit(int cornerNumber);
+        int getNumberOfHitForCorner(int cornerNumber);
+
 
 };
 
